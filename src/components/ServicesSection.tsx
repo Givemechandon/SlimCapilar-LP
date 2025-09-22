f5e3143e52a958f191f60d05d7d1d2d6dffb310d
Givemechandon <brunomalachias2015@gmail.com>
2025-09-22 09:05:40 -0300

    fix: tratamentos

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -69,10 +69,10 @@ const ServicesSection = () => {
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold mb-6">
-            Nossos Procedimentos
+            Nossos Tratamentos
           </h2>
           <p className="text-xl text-muted-foreground max-w-3xl mx-auto">
-            Oferecemos procedimentos capilares especializadas com tecnologia
+            Oferecemos Tratamentos capilares especializadas com tecnologia
             avançada.
           </p>
         </div>
